e07fb02903cdc356c6d2b2c3fb734fa6a013729f
stephenlechner <stephenlechner@gmail.com>
2015-06-01 19:26:16 -0400

    Update media.py
    Modified to meet pep8.

--- media.py
+++ media.py
@@ -1,16 +1,22 @@
 __author__ = 'Steve'
 
-# This file will create a class "movie" that will contain all the data/attributes that the "fresh tomatoes" site will
-# display to the user. Which data/movie attributes are included should be pretty self-explanatory.
-
+# This file defines the classes necessary to render the fresh tomatoes website.
 
 class Movie():
-
-    def __init__(self, movie_title, movie_storyline, movie_poster_img, movie_youtube, movie_imdb):
+    '''
+    This class will structure "movie" objects that will contain all the
+    data/attributes that the "fresh tomatoes" site will display to the user.
+    Which data/movie attributes are included should be pretty self-explanatory.
+    '''
+    def __init__(
+            self, movie_title, movie_storyline, movie_poster_img,
+            movie_youtube, movie_imdb):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_img
         self.trailer_youtube_url = movie_youtube
-        self.imdb_url = movie_imdb  # imdb = "Internet Movie Database", a popular movie critic site.
-        self.fandango = "http://www.fandango.com/search/?q=" + movie_title.lower().replace(" ", "%20")
+        self.imdb_url = movie_imdb
+        # imdb = "Internet Movie Database", a popular movie critic site.
+        self.fandango = ("http://www.fandango.com/search/?q=" +
+                         movie_title.lower().replace(" ", "%20"))
         # Fandango: In case users want to try to buy tickets to the movie.
